e33569945b9667b62e4510da5d8b09690ffc76b7
Zacharia Remy <ziaky0.remo@gmail.com>
2025-05-28 13:14:41 +0200

    Add files via upload

---
+++ Quiz.py
@@ -0,0 +1,45 @@
+import easygui
+
+#Consignes et presentation
+easygui.msgbox("Quiz sur le Hacking Ethique")
+easygui.msgbox("12 Questions + Reponses")
+easygui.msgbox("Revisions + Rappels")
+
+#Questions reponses
+#1
+easygui.msgbox("1. Qu'est-ce qu'un hacker éthique?")
+easygui.msgbox("Réponse: Une personne qui teste légalement la sécurité des systèmes pour y trouver des vulnérabilités avant que des hackers malveillants ne les exploitent.")
+#2
+easygui.msgbox("2. Quel est l'autre nom pour un hacker éthique?")
+easygui.msgbox("Réponse: White Hat (chapeau blanc).")
+#3
+easygui.msgbox("3. Quel protocole est utilisé pour crypter les coommunications web?")
+easygui.msgbox("Réponse: HTTPS.")
+#4
+easygui.msgbox("4. Quel outil Linux est souvent utilisé dans le pentesting?")
+easygui.msgbox("Réponse: Kali Linux")
+#5
+easygui.msgbox("5. Que signifie le terme 'phishing' ?")
+easygui.msgbox("Réponse: Une attaque visant à tromper une victime pour qu'elle divulgue des informations sensibles (mot de passe, données bacaire).")
+#6
+easygui.msgbox("6. Qu'est-ce qu'un teste d'intrusion (pentest)?")
+easygui.msgbox("Réponse: Un test de sécurité pour identifier les failles d'un système informatique.")
+#7
+easygui.msgbox("Quel est le rçole d'un pare-feu?")
+easygui.msgbox("Réponse: Filtrer le trafic réseau entrant et sortant pour protéger les systèmes.")
+#8
+easygui.msgbox("Qu'est-ce qu'une culnérabilité 0-day?")
+easygui.msgbox("Réponse: Une faille inconnue du public et du fabriquant, et donc non corrigée.")
+#9
+easygui.msgbox("Quel est l'outil le plus utilisé pour scanner les ports d'un système?")
+easygui.msgbox("Réponse: Nmap")
+#10
+easygui.msgbox("Que signifie 'Brute Force'?")
+easygui.msgbox("Réponse: Une attaque qui essaie toutes les combinaisons possibles pour trouver un mot de passe.")
+#11
+easygui.msgbox("A quoi sert l'outil 'Wireshark'?")
+easygui.msgbox("Réponse: Analyser les trafic réseau en capturant les paquets.")
+#12
+easygui.msgbox("Qu'est ce que Metasploit?")
+easygui.msgbox("Réponse: Un framework utilisé pour développer et exécuter des exploits contre des machines cibles.")
+
